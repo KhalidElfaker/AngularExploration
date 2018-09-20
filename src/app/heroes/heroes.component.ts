f6ce723cb004136a3eca1928a5d01c1dfa91989f
nisrineServer <nisrine@inovasquad.com>
2018-09-20 12:07:33 +0100

    services and routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,13 +22,18 @@ export class HeroesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  getHeroes(): void {
+  /*getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
+  }*/
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 }
