4f2f9fdac677b01cf26d2ced980c356dfa3671b9
nisrineServer <nisrine@inovasquad.com>
2018-09-21 17:59:55 +0100

    get a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,7 +17,7 @@ export class HeroService {
   constructor(
     private http: HttpClient,
     private messageService: MessageService
-    ) { }
+  ) { }
 
   //getHeros with fake Data (Moak)
   /*
@@ -36,9 +36,10 @@ export class HeroService {
   */
 
   /** GET heroes from the server */
-  getHeroes (): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(heroes => this.log('fetched heroes')),
         catchError(this.handleError('getHeroes', []))
       );
   }
@@ -50,30 +51,40 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 */
+
+/** GET hero by id. Will 404 if id not found */
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
+}
+
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
 
-      /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
-    private handleError<T> (operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
-     
-        // TODO: send the error to remote logging infrastructure
-        console.error(error); // log to console instead
-     
-        // TODO: better job of transforming error for user consumption
-        this.log(`${operation} failed: ${error.message}`);
-     
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
-      };
-    }
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
 }
