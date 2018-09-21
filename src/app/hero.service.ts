927195e3fb8db502bc91811263897cb2c2f421b9
nisrineServer <nisrine@inovasquad.com>
2018-09-21 12:32:32 +0100

    setting up the hero service to use HttpClient instead of RxJs 'of()'

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,21 +9,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  /*getHeroes(): Hero[] {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+  //getHeros with fake Data (Moak)
+  /*
+  getHeroes(): Hero[] {
     return HEROES;
-  }*/
+  }
+  */
 
+  //GEt Hero with a Service from a Moak data
+  /*
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+  */
 
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  /*
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+*/
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
 }
